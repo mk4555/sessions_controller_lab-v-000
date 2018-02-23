8f70a1035e26176d1a256053f7a45d28ea1c5135
Min Kim <mk4555@nyu.edu>
2018-02-23 07:39:06 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  def create
+    if params[:name] && !params[:name].empty?
+      session[:name] = params[:name]
+      redirect_to root_path
+    else
+      redirect_to '/sessions/new'
+    end
+  end
+
+  def destroy
+    session.destroy
+    redirect_to root_path
+  end
+end
